7813a39cf0c4a7d4ed41c0b3ad19325fdf77842e
lynnc00 <167378358+lynnc00@users.noreply.github.com>
2024-05-23 20:30:56 -0400

    Update answers to homework_4.sql

--- 03_homework/homework_4.sql
+++ 03_homework/homework_4.sql
@@ -17,6 +17,13 @@ The `||` values concatenate the columns into strings.
 Edit the appropriate columns -- you're making two edits -- and the NULL rows will be fixed. 
 All the other rows will remain the same.) */
 
+SELECT
+
+product_name || ', ' || 
+IFNULL (product_size, ' ')|| ' (' || 
+IFNULL (product_qty_type, 'unit') || ')'
+
+FROM product
 
 
 
@@ -30,11 +37,124 @@ each new market date for each customer, or select only the unique market dates p
 (without purchase details) and number those visits. 
 HINT: One of these approaches uses ROW_NUMBER() and one uses DENSE_RANK(). */
 
+SELECT DISTINCT
+customer_id
+,market_date
+,dense_rank() OVER (PARTITION BY customer_id ORDER BY market_date ASC) as visit_time
+
+FROM customer_purchases
+
 
 /* 2. Reverse the numbering of the query from a part so each customer’s most recent visit is labeled 1, 
 then write another query that uses this one as a subquery (or temp table) and filters the results to 
 only the customer’s most recent visit. */
 
+--Reverse the numbering of each customer's most recent visit is labeled
+SELECT DISTINCT
+customer_id
+,market_date
+,dense_rank() OVER (PARTITION BY customer_id ORDER BY market_date DESC) as visit_time
+
+FROM customer_purchases
+
+--Output the results of customer's most recent viste date
+SELECT *
+
+FROM
+
+(SELECT DISTINCT
+customer_id
+,market_date
+,dense_rank() OVER (PARTITION BY customer_id ORDER BY market_date DESC) as recent_visit_time
+
+FROM customer_purchases) x
+
+WHERE x.recent_visit_time = 1
 
 /* 3. Using a COUNT() window function, include a value along with each row of the 
 customer_purchases table that indicates how many different times that customer has purchased that product_id. */
+
+SELECT DISTINCT customer_id, product_id
+,COUNT(product_id) OVER (PARTITION BY customer_id, product_id) AS purchase_time
+
+FROM customer_purchases
+
+
+-- String manipulations
+/* 1. Some product names in the product table have descriptions like "Jar" or "Organic". 
+These are separated from the product name with a hyphen. 
+Create a column using SUBSTR (and a couple of other commands) that captures these, but is otherwise NULL. 
+Remove any trailing or leading whitespaces. Don't just use a case statement for each product! 
+
+| product_name               | description |
+|----------------------------|-------------|
+| Habanero Peppers - Organic | Organic     |
+
+Hint: you might need to use INSTR(product_name,'-') to find the hyphens. INSTR will help split the column. */
+
+SELECT product_name
+
+,CASE WHEN INSTR("product_name",'-') > 0 THEN TRIM(SUBSTR (product_name, INSTR("product_name",'-')+1)) 
+ELSE NULL END AS description
+
+FROM product
+
+/* 2. Filter the query to show any product_size value that contain a number with REGEXP. */
+
+SELECT product_name
+
+,CASE WHEN INSTR("product_name",'-') > 0 THEN TRIM(SUBSTR (product_name, INSTR("product_name",'-')+1)) 
+ELSE NULL END AS description
+, product_size
+
+FROM product
+
+WHERE product_size REGEXP '[0-9]'
+
+
+-- UNION
+/* 1. Using a UNION, write a query that displays the market dates with the highest and lowest total sales.
+
+HINT: There are a possibly a few ways to do this query, but if you're struggling, try the following: 
+1) Create a CTE/Temp Table to find sales values grouped dates; 
+2) Create another CTE/Temp table with a rank windowed function on the previous query to create 
+"best day" and "worst day"; 
+3) Query the second temp table twice, once for the best day, once for the worst day, 
+with a UNION binding them. */
+
+--Create a temp table to find sales by different dates
+DROP TABLE IF EXISTS sales_by_date;
+CREATE TEMP TABLE sales_by_date AS
+
+SELECT market_date
+
+,SUM(quantity * cost_to_customer_per_qty) AS sales
+
+FROM customer_purchases
+ 
+ GROUP BY market_date
+
+--Create a temp table for to create 2 columns to rank sales, one is in descending ordering and another one is in ascending ordering.
+DROP TABLE IF EXISTS sales_rank;
+CREATE TEMP TABLE sales_rank AS 
+
+SELECT market_date, sales
+, RANK() OVER (ORDER BY sales DESC) AS sales_rank_desc
+, RANK() OVER (ORDER BY sales ASC) AS sales_rank_asc
+
+FROM sales_by_date
+
+--Select the 'best day' and 'worst day' from sales_rank table based on the maxmium and minmum sales, then union them together.
+SELECT market_date, sales, 'Best Day' as sales_status
+
+FROM sales_rank
+
+WHERE sales_max = 1
+
+UNION
+
+SELECT market_date, sales, 'Worst Day' as sales_status
+
+FROM sales_rank
+
+WHERE sales_min = 1
